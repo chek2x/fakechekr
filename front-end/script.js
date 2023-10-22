1af65c41ad156a05ed14877fd171d2cf64042e79
Leomer Yex <76932467+EXECUTIONER-X@users.noreply.github.com>
2023-10-22 23:25:11 +0800

    Dark mode button works

--- front-end/script.js
+++ front-end/script.js
@@ -2,10 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
   const logo = document.getElementById("logo");
   const searchButton = document.querySelector("button[type='submit']");
   const cover = document.getElementById("cover");
+  const darkModeButton = document.getElementById("dark-mode-button");
+  const body = document.body;
 
+  const isDarkModeEnabled = localStorage.getItem("darkModeEnabled") === "true";
+  
+  function toggleDarkMode() {
+    body.classList.toggle("dark-mode");
+    const darkModeEnabled = body.classList.contains("dark-mode");
+    localStorage.setItem("darkModeEnabled", darkModeEnabled);
+  }
+  
   searchButton.addEventListener("click", function (e) {
     e.preventDefault(); // Prevent form submission
     logo.classList.add("move-left");
     cover.classList.add("move-up"); 
   });
+  darkModeButton.addEventListener("click", toggleDarkMode);
 });
