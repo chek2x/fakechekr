babaf205e1751ced5ac55df0ef5cc750fd333fd7
Chek <zlorenzo@mcm.edu.ph>
2023-10-27 01:24:17 +0800

    publish for online sharing purposes

--- server/website_preprocess.py
+++ server/website_preprocess.py
@@ -1,4 +1,28 @@
-def greetings(name):
-    print(f'Hi, {name}')
+import csv
+import mysql.connector
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-greetings('Jane Doe')
+# Connect to MySQL Local Server
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "NewPassword"
+)
+
+tableCreation = ""
+
+# Navigate local server
+cursor = db.cursor()
+cursor.execute("CREATE TABLE")
+
+# Drop connection to MySQL
+db.close()
+ 
+# url = 'https://www.bbc.com/news/world-europe-67008199'
+ 
+# soup = BeautifulSoup(urlopen(url), features="lxml")
+# header = soup.find('h1')
+
+# print("Title of the article is : ")
+# print (header.text)
