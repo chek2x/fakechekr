aca6a69f376bcd58cee0cddbf59cec46b7830d47
Chek <zlorenzo@mcm.edu.ph>
2023-10-31 22:46:23 +0800

    Update website_preprocess.py

--- server/website_preprocess.py
+++ server/website_preprocess.py
@@ -1,8 +1,14 @@
 import csv
+import nltk
 import re
 import mysql.connector
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+alpha_pattern = re.compile(r'[A-Za-z]+')
+stop_words = set(stopwords.words('english'))
 
 # # Connect to MySQL Local Server
 # db = mysql.connector.connect(
@@ -28,16 +34,35 @@ from bs4 import BeautifulSoup
 # print("Title of the article is : ")
 # print (header.text)
 
+# Preprocess text
+def textCleaner(text) :
+    words = word_tokenize(text)
+    words = [word for word in words if alpha_pattern.match(word)]
+    words = [word for word in words if word.lower() not in stop_words]
+    return ' '.join(words)
+
 # Grab website information
 def webParse(url) -> str:
+    possible_author_class = [re.compile(r'.*contributor[name].*', re.IGNORECASE), re.compile(r'.*author[name].*', re.IGNORECASE), re.compile(r'.*[script]writer[name].*', re.IGNORECASE)]
     soup = BeautifulSoup(urlopen(url).read().decode("utf-8"), "html.parser")
     header = soup.find('h1')
 
     # TODO: Find way to get body using class elements in html and regex for filtering.
-    author = soup.find_all('div', attrs={'class' : re.compile(r'.*contributor.*')}, recursive = True)
+    author = None
+    for class_name in possible_author_class:
+        author = soup.find(['span', 'div', 'p'], class_ = class_name, recursive = True)
+        if author:
+            break
 
-    print("\nTitle:", header.text, "\n")
-    print("Author/s:", author, "\n")
+    # Test for getting
+    print("Title:", textCleaner(header.text), "\n")
+    print("Author/s:", textCleaner(author.text), "\n")
+    print("Body:", textCleaner(), "\n")
 
+
+print("\nTest 1:")
 url = 'https://www.bbc.com/news/world-europe-67008199'
 webParse(url)
+
+print("Test 2:")
+url = 'https'
